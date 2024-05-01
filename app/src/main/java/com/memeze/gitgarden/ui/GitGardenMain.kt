f1f7b5c9e949081471317c553adc26ca8927c81c
Minjae Kim <alswo5052@gmail.com>
2024-05-01 21:19:20 +0900

    GitGarden main composable 추가

---
+++ app/src/main/java/com/memeze/gitgarden/ui/GitGardenMain.kt
@@ -0,0 +1,14 @@
+package com.memeze.gitgarden.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.memeze.gitgarden.ui.theme.GitgardenTheme
+
+
+@Composable
+fun GitGardenMain() {
+    GitgardenTheme {
+        Box(modifier = Modifier)
+    }
+}
