790a55ee1b3f4c41048f5e5d724a2a43cf43833e
Minjae Kim <alswo5052@gmail.com>
2024-05-03 17:59:04 +0900

    Data 모듈에 Okhttp + Retrofit 라이브러리 추가

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -25,6 +25,12 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
+
+    implementation(libs.okhttp.core)
+    implementation(libs.okhttp.logging.interceptor)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.converter.gson)
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
